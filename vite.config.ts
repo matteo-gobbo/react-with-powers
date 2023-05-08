a538d1d837df1c3308bd356e33421fb6fc467a03
Matteo Gobbo <info@matteogobbo.it>
2023-05-08 14:23:15 +0200

    update some configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: true,
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
   plugins: [react()],
 });
